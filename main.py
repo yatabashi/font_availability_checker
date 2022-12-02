4dc138e0e3770fc07c696ef54fee4ec85f795819
yatabashi <ijatabashi@gmail.com>
2022-12-02 23:17:36 +0900

    コマンドライン引数利用を追加

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 # ターミナルから実行できるようにしたい
 
+import os
 import sys
 import udfs
+import argparse
 
-# CUI
+# コマンドライン引数
+# 絶対パス／相対パス？
+parser = argparse.ArgumentParser()
+
+meg = parser.add_mutually_exclusive_group()
+meg.add_argument('-f', '--file', help='specifies a font file checked')
+meg.add_argument('-d', '--directory', help='specifies a directory in which font files checked exist')
+parser.add_argument('text')
+
+args = parser.parse_args()
+
+text = args.text
+if args.file:
+    type = 'file'
+    path = os.path.abspath(args.file)
+elif args.directory:
+    type = 'dir'
+    path = os.path.abspath(args.directory)
+else:
+    type = 'all'
+
+
+# コマンドラインinput
 # text = input('text: ')
 # type = input('file/dir/all: ')
 # if type in ['file', 'dir']:
@@ -13,10 +37,12 @@ import udfs
 # else:
 #     sys.exit('Invalid input')
 
+
 # 直接入力
-text = '/ʔ(ə)ŋ/は[ʔŋ̍]であって[ʔə̩ŋ]ではないよなあと思ってしまっている'
-type = 'file'
-path = '/System/Library/Fonts/Supplemental/AmericanTypewriter.ttc'
+# text = '/ʔ(ə)ŋ/は[ʔŋ̍]であって[ʔə̩ŋ]ではないよなあと思ってしまっている'
+# type = 'file'
+# path = '/System/Library/Fonts/Supplemental/AmericanTypewriter.ttc'
+
 
 # 実行
 if type == 'file':
