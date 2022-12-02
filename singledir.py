debb3090134640b6515d091b59c11f79556d8640
yatabashi <ijatabashi@gmail.com>
2022-12-02 17:41:38 +0900

    処理手順の規格化

--- singledir.py
+++ singledir.py
@@ -1,16 +1,37 @@
+import logging
+from tqdm import tqdm
 import udfs
 
-dirpath = '/Users/kuroiwashu/Library/Fonts'
-text = '/ʔ(ə)ŋ/は[ʔŋ̍]であって[ʔə̩ŋ]ではないよなあと思ってしまっている'
-available_fonts = []
+def main():
+    # 定義
+    dirpath = '/Users/kuroiwashu/Library/Fonts'
+    text = input('Input: ') # /ʔ(ə)ŋ/は[ʔŋ̍]であって[ʔə̩ŋ]ではないよなあと思ってしまっている
+    available_fonts = set()
 
-for filepath in udfs.all_paths(dirpath):
-    tup = udfs.fetch_fontname_and_availability(text, filepath)
+    # fontToolsが警告を出力しないようにする
+    logging.disable(logging.WARNING)
 
-    if tup is not None:
-        fontname, available = tup
-        if available:
-            available_fonts.append(fontname)
+    # tqdmでプログレスバーを表示しながら全ファイルを巡回
+    for filepath in tqdm(udfs.all_paths(dirpath)):
+        # macではこのファイルがエイリアスとしてデフォルトであるらしいので無視
+        if filepath == '/Library/Fonts/Arial Unicode.ttf':
+            continue
+        
+        # 取得
+        # set型に入れて重複を回避
+        fontname_and_availability = udfs.fetch_fontname_and_availability(text, filepath)
 
-for available_font in available_fonts:
-    print(available_font)
+        if fontname_and_availability is not None:
+            fontname, available = fontname_and_availability
+            if available:
+                available_fonts.add(fontname)
+
+    # ソート
+    available_fonts_sorted = sorted(list(available_fonts))
+
+    # 出力
+    for available_font in available_fonts_sorted:
+        print(available_font)
+
+if __name__ == '__main__':
+    main()
--- singlefile.py
+++ singlefile.py
@@ -1,8 +1,23 @@
+import logging
 import udfs
 
-path = '/Library/Fonts/GHEAGrapalatBlit.otf'
-text = 'おはよー'
+def main():
+    # 定義
+    filepath = '/Library/Fonts/GHEAGrapalatBlit.otf'
+    text = input('Input: ') # /ʔ(ə)ŋ/は[ʔŋ̍]であって[ʔə̩ŋ]ではないよなあと思ってしまっている
 
-fontname, isavailable = udfs.fetch_fontname_and_availability(text, path)
+    # fontToolsが警告を出力しないようにする
+    logging.disable(logging.WARNING)
 
-print(f'{fontname} is{"" if isavailable else " NOT"} available for the text.')
+    # 取得
+    fontname_and_availability = udfs.fetch_fontname_and_availability(text, filepath)
+
+    if fontname_and_availability is not None:
+        fontname, isavailable = fontname_and_availability
+        
+        print(f'{fontname} is{"" if isavailable else " NOT"} available for the text.')
+    else:
+        print('unsupported file')
+
+if __name__ == '__main__':
+    main()
