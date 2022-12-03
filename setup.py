59403fbf67b3e79d7521138dbaf1e4540df0f1bc
yatabashi <ijatabashi@gmail.com>
2022-12-04 02:28:46 +0900

    パッケージ化5

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     install_requires = ['fontTools', 'tqdm'],
     packages=['fontac'],
     entry_points = {
-        "console_scripts": ['fontac = package.main:main'], # ['パッケージ名 = ファイル名:関数名']
+        "console_scripts": ['fontac = fontac.main:main'], # ['パッケージ名 = ファイル名:関数名']
     }
 )
