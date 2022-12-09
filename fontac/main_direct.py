8c8bd684137e01d7e88bd81127f46d2e2b421216
yatabashi <ijatabashi@gmail.com>
2022-12-10 02:25:46 +0900

    UX改善

--- fontac/main_direct.py
+++ fontac/main_direct.py
@@ -2,10 +2,10 @@ import logging
 import udfs
 
 # 直接入力
-text = '㐧' # 検索対象の文字列を指定する
+text = '' # 検索対象の文字列を指定する
 type = 'all' # 検索の種類を指定する。'all'は全フォントから利用可能なものを抽出して出力する。'file'は一つの、'dir'は複数のフォントの利用可能性を判定する。
-path = '' # typeが'file'または'dir'の場合、そのフォントのファイルまたはファイルを含むディレクトリを指定する。typeが'all'の場合、この指定は無視される。
-is_verbose = True # typeが'dir'または'all'の場合、各フォントの判定結果を逐一出力するかどうかを指定する（Trueで出力して、Falseで出力しない）。
+path = '' # typeが'file'または'dir'の場合、そのフォントのファイルまたはファイルを含むディレクトリを指定する。
+is_verbose = False # typeが'dir'または'all'の場合、各フォントの判定結果を逐一出力するかどうかを指定する（Trueで出力して、Falseで出力しない）。
 shows_paths = False # typeが'dir'または'all'の場合、該当したフォントのファイルパスも出力するかどうかを指定する（Trueで出力して、Falseで出力しない）。
 
 logging.disable(logging.WARNING)
@@ -17,3 +17,5 @@ elif type == 'dir':
     udfs.main_for_dir(text, path, is_verbose, shows_paths)
 elif type == 'all':
     udfs.main_for_allfonts(text, is_verbose, shows_paths)
+else:
+    print('invalid value set to `type`')
